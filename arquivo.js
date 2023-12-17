cda0c38aab68c8112fc6e6d8c718249ef6c3376a
seu-username <seu-email>
2023-12-17 10:53:21 -0300

    pronto

--- arquivo.js
+++
@@ -1,6 +0,0 @@
-[{
-  "rodada": 1, "jogos": [{ "team_home_id": "tempo-a", "team_home_name": "tempo A", "team_home_score": 3, "team_away_id": "time-h", "team_away_name": "Time H", "team_away_score": 5 },
-  { "team_home_id": "time-b", "team_home_name": "Time B", "team_home_score": 4, "team_away_id": "time-g", "team_away_name": "Tempo G", "team_away_score": 3 },
-  { "team_home_id": "time-c", "team_home_name": "Tempo C", "team_home_score": 1, "team_away_id": "time-f", "team_away_name": "Tempo F", "team_away_score": 5 },
-  { "team_home_id": "time-d", "team_home_name": "Time D", "team_home_score": 0, "team_away_id": "time-e", "team_away_name": " ;Tempo E", "team_away_score": 5 }]
-}]
